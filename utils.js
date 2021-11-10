86d6033e06a2e626408f1a4940c7d8330085b079
theshadowagent <shadowagent1st@gmail.com>
2021-11-10 18:37:33 +0300

    Fixed error parsing

--- utils.js
+++ utils.js
@@ -11,10 +11,18 @@ export const objectMap = (object, mapFn) => {
 export const normalizeURL = (u) => ((new URL(u).host).replace('www.', ''))
 
 export const parseTxError = (error) => {
-    return {
-        code: error.code ?? JSON.parse(`{${error.message.split("{")[1]}`).code,
-        message: error.message.split("{")[0].trim()
-    };
+    try {
+        return {
+            code: error.code ?? JSON.parse(`{${error.message.split("{")[1]}`).code,
+            message: error.message.split("{")[0].trim()
+        };
+    } catch (parse_error) {
+        console.log("Failed to parse error code and message")
+        console.log("Original error:", error)
+        return {
+            code: undefined, message: error.toString()
+        }
+    }
 }
 
 // Avoid big number errors without using external libraries
