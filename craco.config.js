ca409dfbe3ca67106d33c5fe424c16cf86164da7
theshadowagent <shadowagent1st@gmail.com>
2021-12-07 02:49:39 +0800

    Fixed webpack

--- craco.config.js
+++ craco.config.js
@@ -4,19 +4,17 @@ module.exports = {
         configure: (webpackConfig, {env, paths}) => {
             const isEnvProduction = env === "production";
             const isEnvDevelopment = env === "development";
+            webpackConfig.output.libraryTarget = "umd";
             webpackConfig.output.library = "NFTComponents";
             webpackConfig.output.filename = isEnvProduction
                 ? 'static/js/[name].js'
                 : isEnvDevelopment && 'static/js/bundle.js';
-            webpackConfig.output.chunkFilename = isEnvProduction
-                ? 'static/js/[name].js'
-                : isEnvDevelopment && 'static/js/[name].js';
-            // webpackConfig.resolve.plugins = webpackConfig.resolve.plugins
-            //     .filter(p => p.name !== 'ModuleScopePlugin')
+            // Turn off chunking
+            webpackConfig.optimization = {};
+            // Remove ModuleScopePlugin
             const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
                 ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
             );
-
             webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
             return webpackConfig;
         }
