728c04738c66cdb3c2c2937d6c8a6db276d384c0
shad <shadowagent1st@gmail.com>
2022-05-27 02:10:09 +0800

    fix: added a setting for manually setting public mint price, new mint method variant

--- src/mint/web3.js
+++ src/mint/web3.js
@@ -28,13 +28,28 @@ const getMintTx = ({ numberOfTokens, ref, tier, wallet }) => {
         return NFTContract.methods.mint(tier, numberOfTokens, ref ?? wallet);
     }
     console.log("Using hardcoded mint method detection")
-    const methodNameVariants = ['mint', 'publicMint', 'mintNFTs']
+    const methodNameVariants = ['mint', 'publicMint', 'mintNFTs', 'mintPublic']
     const name = methodNameVariants.find(n => findMethodByName(n) !== undefined)
     if (!name) {
         alert("Buildship widget doesn't know how to mint from your contract. Contact https://buildship.xyz in Discord to resolve this.")
         return undefined
     }
     return NFTContract.methods[findMethodByName(name)](numberOfTokens);
+}
+
+const getDefaultMintPrice = () => {
+    // for contracts without exported price variable or method
+    const defaultPrice = window.DEFAULTS?.publicMint?.price
+    if (defaultPrice) {
+        const priceNumber = typeof defaultPrice === "string" ? Number(defaultPrice) : defaultPrice
+        if (isNaN(priceNumber)) {
+            alert("Wrong publicMintPrice format, should be a number in ETH (or native token)")
+            return undefined
+        }
+        console.warn("Using DEFAULTS.publicMint.price as price not found in the smart-contract")
+        return (priceNumber * 1e18).toString()
+    }
+    return undefined
 }
 
 const getMintPrice = async (tier) => {
@@ -51,15 +66,22 @@ const getMintPrice = async (tier) => {
             console.log("Using price method auto-detection")
             return NFTContract.methods[matches[0]]().call()
         case 0:
-            alert("Buildship widget doesn't know how to fetch price from your contract. Contact https://buildship.xyz in Discord to resolve this.")
-            return undefined
+            const defaultMintPrice = getDefaultMintPrice()
+            if (defaultMintPrice === undefined) {
+                alert("Buildship widget doesn't know how to fetch price from your contract. Contact https://buildship.xyz in Discord to resolve this.")
+            }
+            return defaultMintPrice
         default:
             console.log("Using hardcoded price detection")
             const methodNameVariants = ['price', 'cost', 'public_sale_price', 'getPrice']
             const name = methodNameVariants.find(n => findMethodByName(n) !== undefined)
             if (!name) {
-                alert("Buildship widget doesn't know how to fetch price from your contract. Contact https://buildship.xyz in Discord to resolve this.")
-                return undefined
+                const defaultMintPrice = getDefaultMintPrice()
+                console.log("defaultMintPrice", defaultMintPrice)
+                if (defaultMintPrice === undefined) {
+                    alert("Buildship widget doesn't know how to fetch price from your contract. Contact https://buildship.xyz in Discord to resolve this.")
+                }
+                return defaultMintPrice
             }
             return NFTContract.methods[findMethodByName(name)]().call();
     }
