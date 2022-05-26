33f4161c435034dc122aa46b391fb836b5e6986e
shad <shadowagent1st@gmail.com>
2022-05-27 04:35:11 +0800

    feat: added public blacklist

--- src/mint/index.js
+++ src/mint/index.js
@@ -1,8 +1,15 @@
 import { updateMintButton, updateMintByTierButtons, updateMintedCounter } from "./ui.js";
 import { updateConnectButton, updateWalletStatus } from "../wallet.js";
 import { setContracts } from "../contract.js";
+import { blacklist } from "./blacklist";
 
 export const init = async () => {
+    if (blacklist.includes(window.location.host.replace('www.', ''))) {
+        alert("This website is blacklisted by Buildship minting widget as it's a scam. " +
+            "You shouldn't mint as it's pretending to be another project")
+        return
+    }
+
     await updateWalletStatus();
     await setContracts();
     updateConnectButton();
