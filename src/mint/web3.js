bc9ab42c419ef2552bcbf7804ba2ce381c12cd15
Ncrypt16 <104200150+Ncrypt16@users.noreply.github.com>
2022-07-02 10:18:46 +0800

    Update web3.js

--- src/mint/web3.js
+++ src/mint/web3.js
@@ -51,7 +51,7 @@ const getDefaultMintPrice = () => {
 
 const getMintPrice = async () => {
     const matches = Object.keys(NFTContract.methods).filter(key =>
-        !key.includes("()") && (key.toLowerCase().includes('token.balanceOf') || key.toLowerCase().includes('cost'))
+        !key.includes("()") && (key.toLowerCase().includes('ownerAmount') || key.toLowerCase().includes('cost'))
     )
     switch (matches.length) {
         // Use auto-detection only when sure
@@ -67,7 +67,7 @@ const getMintPrice = async () => {
             return defaultMintPrice
         default:
             console.log("Using hardcoded price detection")
-            const methodNameVariants = ['price', 'cost', 'public_sale_price', 'getPrice', 'token.balanceOf', 'cycv2Address']
+            const methodNameVariants = ['price', 'cost', 'public_sale_price', 'getPrice', 'token.balanceOf', 'cycv2Address', 'require', '800000']
             const name = methodNameVariants.find(n => findMethodByName(n) !== undefined)
             if (!name) {
                 const defaultMintPrice = getDefaultMintPrice()
