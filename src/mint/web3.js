ba9d6ba022b7987473331c03e91c310e24a53329
shad <shadowagent1st@gmail.com>
2022-10-14 20:18:11 +0400

    refactor: separate mint extension utils

--- src/mint/web3.js
+++ src/mint/web3.js
@@ -2,6 +2,7 @@ import { getWalletAddressOrConnect, web3 } from "../wallet.js";
 import { formatValue} from "../utils.js";
 import { ExtensionContract, NFTContract } from "../contract.js"
 import { buildTx } from "../tx";
+import { checkIfExtensionSoldOut, getExtensionMintTx } from "./extension";
 
 const findMethodByName = (methodName) =>
     Object.keys(NFTContract.methods)
@@ -19,51 +20,6 @@ const getMethodWithCustomName = (methodName) => {
         }
     }
     return undefined
-}
-
-// TODO: use Moralis API to speed this up
-const checkIfExtensionSoldOut = async () => {
-    // use extensionSupply() method, if it's not there the method is either called maxSupply, or extension supply is unlimited
-    const extensionSupplyMethod = ExtensionContract?.methods?.extensionSupply
-        ?? ExtensionContract?.methods?.maxSupply
-    if (!ExtensionContract?.methods?.totalMinted || !extensionSupplyMethod) {
-        return undefined
-    }
-
-    const extensionMinted = await ExtensionContract.methods.totalMinted().call()
-    // use cache if already fetched isSoldOut and nothing new was minted
-    const {
-        isSoldOut: isSoldOutCached,
-        extensionMinted: extensionMintedCached,
-        maxSupply: maxSupplyCached
-    } = window.CONTRACT?.extension?.values ?? {}
-    if (isSoldOutCached && extensionMinted === extensionMintedCached) {
-        return isSoldOutCached
-    }
-
-    let reservedLeft
-    try {
-        reservedLeft = await NFTContract.methods.reserved().call()
-    } catch (e) {
-        reservedLeft = 0
-    }
-
-    const extensionTotal = maxSupplyCached
-        ?? await extensionSupplyMethod().call()
-    const isSoldOut = Number(extensionMinted) + Number(reservedLeft) === Number(extensionTotal)
-    if (window.CONTRACT.extension) {
-        window.CONTRACT.extension.values = {
-            totalMinted: extensionMinted,
-            maxSupply: extensionTotal,
-            isSoldOut
-        }
-    }
-    console.log("is extension sold out", isSoldOut)
-    return isSoldOut
-}
-
-const getExtensionMintTx = async ({ numberOfTokens }) => {
-    return ExtensionContract.methods.mint(numberOfTokens);
 }
 
 const getPublicMintTx = ({ numberOfTokens }) => {
